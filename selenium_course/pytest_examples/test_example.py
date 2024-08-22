6b2c7f71c151983ac57da38311f3125693035c45
Homel Kanstantsin <homelkv@gmail.com>
2024-08-22 17:09:23 +0300

    unittest pytest

---
+++ selenium_course/pytest_examples/test_example.py
@@ -0,0 +1,9 @@
+import pytest
+
+class TestAbs():
+    def test_abs1(self):
+        assert abs(-42) == 42, "Should be absolute value of a number"
+
+    def test_abs2(self):
+        assert abs(-42) == -42, "Should be absolute value of a number"
+
