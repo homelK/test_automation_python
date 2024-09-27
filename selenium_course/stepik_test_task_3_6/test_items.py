bcbe77f0af489d4fa0e0e0f8ac791e8ac3d32ca8
homelK <homelkv@gmail.com>
2024-09-27 14:53:41 +0300

    new branch api_testing

--- selenium_course/stepik_test_task_3_6/test_items.py
+++
@@ -1,8 +0,0 @@
-import time
-from selenium.webdriver.common.by import By
-
-def test_add_to_basket_btn_exists(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.get(link)
-    # time.sleep(30)
-    assert browser.find_element(By.XPATH, "//button[contains(@class, 'btn-add-to-basket')]").is_displayed(), "The button 'add to basket' is not found"
